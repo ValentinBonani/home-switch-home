e0be46a72313483bd9d8f703f0b42b927ad4937c
ValentinBonani <valentin.bonani@gmail.com>
2019-05-18 16:03:01 -0300

    fix merge

--- routes.js
+++ routes.js
@@ -7,22 +7,8 @@ const {
     PaginasAdminController
 } = require("./controllers");
 
-<<<<<<< HEAD
-  const auth = require("./middleware/auth");
-  const authAdmin = require("./middleware/authAdmin");
-  
-  function mapGenericControllerRoutes(controllers, router) {
-    controllers.forEach(({basePath, controller}) => {
-      router.route(basePath)
-        .get(controller.list)
-        .post(controller.create);
-  
-      router.route(`${basePath}/:id`)
-        .get(controller.read)
-        .put(controller.update)
-        .delete(controller.remove);
-=======
-const auth = require("./middleware/auth")
+const auth = require("./middleware/auth");
+const authAdmin = require("./middleware/authAdmin");
 
 function mapGenericControllerRoutes(controllers, router) {
     controllers.forEach(({ basePath, controller }) => {
@@ -34,7 +20,6 @@ function mapGenericControllerRoutes(controllers, router) {
             .get(controller.read)
             .post(controller.update)
             .delete(controller.remove);
->>>>>>> 562d89e20e4cfff231cc84a6e75f8e34f21542f4
     });
 }
 
@@ -69,30 +54,20 @@ module.exports = (app, router) => {
         .post(authenticateController.authenticate);
 
     router.route("/admin")
-<<<<<<< HEAD
       .get(paginasAdminController.renderLoginAdmin);
     
       router.route("/admin/home")
       .get(authAdmin, paginasAdminController.renderAdminHome);
-=======
-        .get(paginasAdminController.renderLoginAdmin);
-
-    router.route("/admin/home")
-        .get(paginasAdminController.renderAdminHome);
->>>>>>> 562d89e20e4cfff231cc84a6e75f8e34f21542f4
 
     router.route("/admin/authenticate")
         .post(authenticateController.authenticateAdmin);
 
     router.route("/admin/add-property")
-<<<<<<< HEAD
       .get(authAdmin, paginasAdminController.renderAddProperty);
 
-=======
-        .get(paginasAdminController.renderAddProperty);
     router.route("/admin/edit-property/:id")
         .get(paginasAdminController.renderEditProperty);
->>>>>>> 562d89e20e4cfff231cc84a6e75f8e34f21542f4
+
     router.get('/logout', authenticateController.logout);
     return router;
 };
